175acb5b703bfc40d08aa44ebf3a5f8f74c80fb5
kimjaeseop <dkfzm3221@naver.com>
2021-09-11 18:47:45 +0900

    2021-09-11

---
+++ src/main/java/com/board/dao/BoardDAOImpl.java
@@ -0,0 +1,47 @@
+package com.board.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.board.domain.BoardVO;
+
+@Repository
+public class BoardDAOImpl implements BoardDAO {
+
+	@Inject
+	private SqlSession sql;
+ 
+	private static String namespace = "com.board.mappers.board";
+
+	// 게시물 목록
+	@Override
+	public List<BoardVO> list() throws Exception { 
+  
+		return sql.selectList(namespace + ".list");
+ }
+
+	@Override
+	public void write(BoardVO vo) throws Exception {
+		sql.insert(namespace + ".write", vo);
+	}
+	
+	@Override
+	public BoardVO view(int bno) throws Exception {
+		return sql.selectOne(namespace + ".view", bno);
+	}
+	
+	@Override
+	public void modify(BoardVO vo) throws Exception { 
+		sql.update(namespace + ".modify", vo);
+	}
+	
+	@Override
+	public void delete(int bno) throws Exception {
+		sql.delete(namespace + ".delete", bno);
+	}
+
+}

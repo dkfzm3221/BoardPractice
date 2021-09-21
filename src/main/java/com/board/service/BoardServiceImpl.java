614bbce7b87f60af0a3ae6fa9df970cd41e4928e
kimjaeseop <dkfzm3221@naver.com>
2021-09-21 14:41:55 +0900

    2021-09-21

--- src/main/java/com/board/service/BoardServiceImpl.java
+++ src/main/java/com/board/service/BoardServiceImpl.java
@@ -26,7 +26,7 @@ public class BoardServiceImpl implements BoardService {
 	public void write(BoardVO vo) throws Exception {
 		dao.write(vo);
 	}
-	
+	  
 	//게시물 조회
 	@Override
 	public BoardVO view(int bno) throws Exception {
